f8e1389a0cceee9456834100b3e4c46afff46139
davidp799 <papdav12@gmail.com>
2021-10-31 20:31:46 -0400

    application layout

--- app/src/main/java/com/davidp799/patcotoday/MainActivity.java
+++ app/src/main/java/com/davidp799/patcotoday/MainActivity.java
@@ -3,12 +3,26 @@ package com.davidp799.patcotoday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
+
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else { super.onBackPressed(); }
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("http://www.ridepatco.org/schedules/schedules.asp");
+
     }
+
 }
