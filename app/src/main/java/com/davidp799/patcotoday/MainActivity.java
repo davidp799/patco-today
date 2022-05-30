7e3535fd8d4b629a60278283258c057c602f9dba
davidp799 <papdav12@gmail.com>
2022-05-30 03:11:26 -0400

    add splash screen and fix getspecial

--- app/src/main/java/com/davidp799/patcotoday/MainActivity.java
+++ app/src/main/java/com/davidp799/patcotoday/MainActivity.java
@@ -1,5 +1,6 @@
 package com.davidp799.patcotoday;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
 
