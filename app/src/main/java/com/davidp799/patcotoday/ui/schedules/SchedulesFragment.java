a0c8ab06a847e816657b375af0199b5965c4f131
davidp799 <papdav12@gmail.com>
2022-07-03 16:26:53 -0400

    update gradle dependencies

--- app/src/main/java/com/davidp799/patcotoday/ui/schedules/SchedulesFragment.java
+++ app/src/main/java/com/davidp799/patcotoday/ui/schedules/SchedulesFragment.java
@@ -71,69 +71,22 @@ public class SchedulesFragment extends Fragment {
     private int fromSelection, toSelection;
     private boolean internet, special, downloaded, converted, parsed;
     private static final String directory = "/data/data/com.davidp799.patcotoday/files/data/";
-    private ArrayList<String> specialWestBound = new ArrayList<>();
-    private ArrayList<String> specialEastBound = new ArrayList<>();
-    private ArrayList<String> specialURL = new ArrayList<>();
-    private ArrayList<String> specialText = new ArrayList<>();
-    private ArrayList<Arrival> specialArrivals = new ArrayList<>();
+    private final ArrayList<String> specialWestBound = new ArrayList<>();
+    private final ArrayList<String> specialEastBound = new ArrayList<>();
+    private final ArrayList<String> specialURL = new ArrayList<>();
+    private final ArrayList<String> specialText = new ArrayList<>();
+    private final ArrayList<Arrival> specialArrivals = new ArrayList<>();
     // bg lists
-    private ArrayList<String> specialURLs = new ArrayList<>();
-    private ArrayList<String> specialTexts = new ArrayList<>();
-    private ArrayList<String> runnableConvertedStrings = new ArrayList<>();
-    private ArrayList<ArrayList<String>> parsedArrivals = new ArrayList<>();
+    private final ArrayList<String> specialURLs = new ArrayList<>();
+    private final ArrayList<String> specialTexts = new ArrayList<>();
+    private final ArrayList<String> runnableConvertedStrings = new ArrayList<>();
+    private final ArrayList<ArrayList<String>> parsedArrivals = new ArrayList<>();
     //
     private Document doc;
     private final Schedules schedules = new Schedules();
     private static ConnectivityManager connectivityManager;
     private final int weekday = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)-1; // weekday in java starts on sunday
 
-    // Initialize Thread Handlers
-    Handler downloadHandler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Bundle downloadBundle = msg.getData();
-            downloaded = downloadBundle.getBoolean("MSG_KEY");
-        }
-    };
-    Handler internetHandler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            internet = msg.getData().getBoolean("MSG_KEY");
-        }
-    };
-    Handler specialHandler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Bundle specialBundle = msg.getData();
-            special = specialBundle.getBoolean("MSG_BOOLEAN");
-            specialURL = specialBundle.getStringArrayList("MSG_URL");
-            specialText = specialBundle.getStringArrayList("MSG_TEXT");
-            if (!special) {
-                Toast.makeText(getActivity(), "No Special Schedules Today", Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
-    Handler convertHandler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Bundle convertBundle = msg.getData();
-            converted = convertBundle.getBoolean("MSG_BOOLEAN");
-        }
-    };
-    Handler parseHandler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Bundle parseBundle = msg.getData();
-            specialWestBound = parseBundle.getStringArrayList("MSG_WB");
-            specialEastBound = parseBundle.getStringArrayList("MSG_EB");
-            parsed = parseBundle.getBoolean("MSG_KEY");
-        }
-    };
     /* Initialize onCreate */
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSchedulesBinding.inflate(inflater, container, false);
@@ -377,216 +330,6 @@ public class SchedulesFragment extends Fragment {
                 } value = i+1;
             } catch (ParseException e) { e.printStackTrace(); }
         } return value;
-    }
-    /* Background Tasks using Runnable */
-    public void backgroundTasks(String destinationFilePath) {
-        /* Initialize common variables and assets */
-        ArrayList<String> specialTexts = new ArrayList<>();
-        ArrayList<String> specialURLs = new ArrayList<>();
-        ArrayList<String> runnableConvertedStrings = new ArrayList<>();
-        ArrayList<ArrayList<String>> parsedArrivals = new ArrayList<>();
-
-        Runnable internetRunnable = new Runnable() {
-            final Message internetMessage = internetHandler.obtainMessage();
-            final Bundle internetBundle = new Bundle();
-
-            @Override
-            public void run() {
-                try {
-                    connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) { // use new connectivity manager mode if Android N
-                        connectivityManager.registerDefaultNetworkCallback(new ConnectivityManager.NetworkCallback() {
-                            @Override
-                            public void onAvailable(@NonNull Network network) {
-                                internet = true;
-                            }
-
-                            @Override
-                            public void onLost(@NonNull Network network) {
-                                internet = false;
-                            }
-                        });
-                    } else { // otherwise, ping server
-                        try {
-                            String command = "ping -c 1 www.ridepatco.org";
-                            internet = (Runtime.getRuntime().exec(command).waitFor() == 0);
-                        } catch (Exception e) {
-                            internet = false;
-                        }
-                    }
-                } catch (Exception e) {
-                    internet = false;
-                }
-                internetBundle.putBoolean("MSG_KEY", internet);
-                internetMessage.setData(internetBundle);
-                internetHandler.sendMessage(internetMessage);
-            }
-        };
-
-        Runnable specialRunnable = new Runnable() {
-            final Message specialMessage = specialHandler.obtainMessage();
-            final Bundle specialBundle = new Bundle();
-
-            @Override
-            public void run() {
-                try {
-                    doc = Jsoup.connect("http://www.ridepatco.org/schedules/schedules.asp").get();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // get special status
-                GetSpecial getSpecial = new GetSpecial(doc);
-                specialURLs.addAll(getSpecial.getUrl());
-                specialTexts.addAll(getSpecial.getText());
-                specialBundle.putBoolean("MSG_BOOLEAN", getSpecial.getStatus());
-                specialBundle.putStringArrayList("MSG_URL", specialURLs);
-                specialBundle.putStringArrayList("MSG_TEXT", specialTexts);
-                specialMessage.setData(specialBundle);
-                specialHandler.sendMessage(specialMessage);
-            }
-        };
-        Runnable downloadRunnable = new Runnable() {
-            final Message downloadMessage = downloadHandler.obtainMessage();
-            final Bundle downloadBundle = new Bundle();
-
-            @Override
-            public void run() {
-                InputStream input = null;
-                OutputStream output = null;
-                HttpURLConnection connection = null;
-                for (int i=0; i<specialURLs.size(); i++) {
-                    String filePath = directory + "/special/" + "special" + i + ".pdf";
-                    System.out.println(specialTexts.get(i));
-                    System.out.println(specialURLs.get(i));
-                    try {
-                        URL url = new URL(specialURLs.get(i));
-                        connection = (HttpURLConnection) url.openConnection();
-                        connection.connect();
-                        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                            Log.d("downloadPDF", "Server ResponseCode=" + connection.getResponseCode() + " ResponseMessage=" + connection.getResponseMessage());
-                        }
-                        // download the file
-                        input = connection.getInputStream();
-                        Log.d("downloadPDF", "destinationFilePath=" + filePath);
-                        File newFile = new File(filePath);
-                        newFile.getParentFile().mkdirs();
-                        newFile.createNewFile();
-                        output = new FileOutputStream(filePath);
-
-                        byte[] data = new byte[4096];
-                        int count;
-                        while ((count = input.read(data)) != -1) {
-                            output.write(data, 0, count);
-                        }
-                        downloaded = true;
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        return;
-                    } finally {
-                        try {
-                            if (output != null) output.close();
-                            if (input != null) input.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        if (connection != null) connection.disconnect();
-                    }
-                    File f = new File(destinationFilePath);
-                    Log.d("downloadPDF", "f.getParentFile().getPath()=" + f.getParentFile().getPath());
-                    Log.d("downloadPDF", "f.getName()=" + f.getName().replace(".pdf", ""));
-                }
-                downloadBundle.putBoolean("MSG_KEY", downloaded);
-                downloadMessage.setData(downloadBundle);
-                downloadHandler.sendMessage(downloadMessage);
-            }
-        };
-        /* Runnable Thread for conversion of pdf data */
-        Runnable convertRunnable = new Runnable() {
-            final Message convertMessage = convertHandler.obtainMessage();
-            final Bundle convertBundle = new Bundle();
-
-            @Override
-            public void run() {
-                try {
-                    for (int i = 0; i < specialURLs.size(); i++) {
-                        System.out.println("- Converting: special" + i + ".pdf");
-                        ConvertPDF convertPDF = new ConvertPDF(directory + "/special/", "special" + i + ".pdf");
-                        System.out.println("- PDF" + i + " Characters = " + convertPDF.getText().length());
-                        runnableConvertedStrings.add(convertPDF.getText());
-                    }
-                    converted = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                convertBundle.putStringArrayList("MSG_CONVERTED", runnableConvertedStrings); // send arraylist to handler
-                convertBundle.putBoolean("MSG_BOOLEAN", converted); // send converted status to handler
-                convertMessage.setData(convertBundle);
-                convertHandler.sendMessage(convertMessage);
-            }
-        };
-        Runnable parseRunnable = new Runnable() {
-            final Message parseMessage = parseHandler.obtainMessage();
-            final Bundle parseBundle = new Bundle();
-
-            @Override
-            public void run() {
-                try {
-                    for (int i = 0; i < runnableConvertedStrings.size(); i++) {
-                        System.out.println("- Parsing Text" + i + ":");
-                        System.out.println("-----------------------------------------------------------------------");
-                        ParsePDF parsePDF = new ParsePDF(runnableConvertedStrings.get(i));
-                        parsedArrivals.addAll(parsePDF.getArrivalLines());
-                        for (int j = 0; j < parsedArrivals.size(); j++) {
-                            if (j == 0) {
-                                System.out.println("Westbound:");
-                                System.out.println(parsedArrivals.get(j));
-                            } else {
-                                System.out.println("Eastbound:");
-                                System.out.println(parsedArrivals.get(j));
-                            }
-                        }
-                        System.out.println("-----------------------------------------------------------------------");
-                    }
-                    parsed = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    parsed = false;
-                }
-                if (parsedArrivals.size() > 0) {
-                    parseBundle.putStringArrayList("MSG_WB", parsedArrivals.get(0));
-                } if (parsedArrivals.size() > 1) {
-                    parseBundle.putStringArrayList("MSG_EB", parsedArrivals.get(1));
-                }
-                parseBundle.putBoolean("MSG_KEY", parsed);
-                parseMessage.setData(parseBundle);
-                parseHandler.sendMessage(parseMessage);
-            }
-        };
-
-        try {
-            // First thread
-            Thread t1_internet = new Thread(internetRunnable);
-            t1_internet.start();
-            t1_internet.join();
-            // Second thread
-            Thread t2_special = new Thread(specialRunnable);
-            t2_special.start();
-            t2_special.join();
-            // Third thread
-            Thread t3_download = new Thread(downloadRunnable);
-            t3_download.start();
-            t3_download.join();
-            // Fourth thread
-            Thread t4_convert = new Thread(convertRunnable);
-            t4_convert.start();
-            t4_convert.join();
-            // Fifth thread
-            Thread t5_parse = new Thread(parseRunnable);
-            t5_parse.start();
-            t5_parse.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
     /* Background Tasks using ExecutorService */
     public void checkInternetBg() {
