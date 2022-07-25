bba1706b679cebc40118f79ac4e5668970d4dc2c
David <papdav12@gmail.com>
2022-07-25 12:08:33 -0400

    streamline code and imports

--- app/src/main/java/com/davidp799/patcotoday/ui/schedules/SchedulesViewModel.kt
+++ app/src/main/java/com/davidp799/patcotoday/ui/schedules/SchedulesViewModel.kt
@@ -1,11 +1,18 @@
 package com.davidp799.patcotoday.ui.schedules
 
 import androidx.lifecycle.ViewModel
+import com.davidp799.patcotoday.R
 import com.davidp799.patcotoday.utils.Arrival
 import com.davidp799.patcotoday.utils.Schedules
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SchedulesViewModel : ViewModel() {
     // Station options
+    // Station Data
+    var weekday =
+        Calendar.getInstance()[Calendar.DAY_OF_WEEK] - 1 // weekday in java starts on sunday
     var fromSelection = 0
     var toSelection = 0
     // directory
@@ -17,6 +24,7 @@ class SchedulesViewModel : ViewModel() {
     var converted = false
     var parsed = false
     var schedules = Schedules()
+
     // Background Lists
     var specialEastBound = ArrayList<String>()
     var specialWestBound = ArrayList<String>()
