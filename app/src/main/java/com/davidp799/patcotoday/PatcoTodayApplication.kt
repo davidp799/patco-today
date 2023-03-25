36f29cb226046083b65cd4354e7716aa9df4d14a
davidp799 <papdav12@gmail.com>
2023-03-25 18:56:55 -0400

    add switch for dynamic theming

--- app/src/main/java/com/davidp799/patcotoday/PatcoTodayApplication.kt
+++ app/src/main/java/com/davidp799/patcotoday/PatcoTodayApplication.kt
@@ -1,11 +1,17 @@
 package com.davidp799.patcotoday
 
 import android.app.Application
+import android.content.Context
 import com.google.android.material.color.DynamicColors
 
 class PatcoTodayApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-//        DynamicColors.applyToActivitiesIfAvailable(this)
+        // shared preferences
+        val sharedPreferences = getSharedPreferences("com.davidp799.patcotoday_preferences", Context.MODE_PRIVATE)
+        val isDynamic = sharedPreferences.getBoolean("dynamic_colors", false)
+        if (isDynamic) {
+            DynamicColors.applyToActivitiesIfAvailable(this)
+        }
     }
 }
