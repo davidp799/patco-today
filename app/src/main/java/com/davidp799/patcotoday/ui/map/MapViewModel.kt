98be2e971da37771eae976270402f36d51c5d4a1
David <papdav12@gmail.com>
2025-04-02 12:40:22 -0400

    remove unused imports and vars

--- app/src/main/java/com/davidp799/patcotoday/ui/map/MapViewModel.kt
+++ app/src/main/java/com/davidp799/patcotoday/ui/map/MapViewModel.kt
@@ -9,23 +9,5 @@ class MapViewModel : ViewModel() {
         "City Hall", "Franklin Square", "8th & Market", "9/10th & Locust",
         "12/13th & Locust", "15/16th & Locust"
     )
-    private val _stationLinks = arrayOf(
-        "https://www.ridepatco.org/stations/lindenwold.asp",
-        "https://www.ridepatco.org/stations/ashland.asp",
-        "https://www.ridepatco.org/stations/woodcrest.asp",
-        "https://www.ridepatco.org/stations/haddonfield.asp",
-        "https://www.ridepatco.org/stations/westmont.asp",
-        "https://www.ridepatco.org/stations/collingswood.asp",
-        "https://www.ridepatco.org/stations/ferryave.asp",
-        "https://www.ridepatco.org/stations/broadway.asp",
-        "https://www.ridepatco.org/stations/cityhall.asp",
-        "https://www.ridepatco.org/stations/franklinsquare.asp",
-        "https://www.ridepatco.org/stations/8th.asp",
-        "https://www.ridepatco.org/stations/9th.asp",
-        "https://www.ridepatco.org/stations/12th.asp",
-        "https://www.ridepatco.org/stations/15th.asp"
-    )
-
     val stationList = _stationList
-    val stationLinks = _stationLinks
 }
--- app/src/main/java/com/davidp799/patcotoday/utils/Schedules.java
+++ app/src/main/java/com/davidp799/patcotoday/utils/Schedules.java
@@ -1,9 +1,6 @@
 package com.davidp799.patcotoday.utils;
-
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
