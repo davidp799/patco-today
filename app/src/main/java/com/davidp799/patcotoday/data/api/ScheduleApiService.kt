9f223d9d6f15141873913ae8f6a387a125dfa3cc
David <papdav12@gmail.com>
2025-07-22 15:12:00 -0400

    feat: add ScheduleApiService for fetching schedules from the API

---
+++ app/src/main/java/com/davidp799/patcotoday/data/api/ScheduleApiService.kt
@@ -0,0 +1,16 @@
+package com.davidp799.patcotoday.data.api
+
+import com.davidp799.patcotoday.data.models.ApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface ScheduleApiService {
+    @GET("/")
+    suspend fun getSchedules(
+        @Query("schedule_date") scheduleDate: String,
+        @Query("last_updated") lastUpdated: String,
+        @Header("x-api-key") apiKey: String
+    ): Response<ApiResponse>
+}
