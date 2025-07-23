313e4927068baa3ef7f55bdf35521846b0928e63
David <papdav12@gmail.com>
2025-07-23 12:20:53 -0400

    feat: add preview for SettingsScreen component

--- app/src/main/java/com/davidp799/patcotoday/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/davidp799/patcotoday/ui/screens/SettingsScreen.kt
@@ -14,8 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,4 +56,10 @@ fun SettingsScreen(navController: NavController) {
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreenPreview() {
+    SettingsScreen(navController = rememberNavController())
 }
